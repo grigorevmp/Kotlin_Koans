5e0ff748288a99e16a584199f068fecb092560c7
Mikhail <16112000m@gmail.com>
2021-10-26 13:07:35 +0300

    fixes

--- Classes/Extension functions.kt
+++
@@ -1,5 +0,0 @@
-fun Int.r(): RationalNumber = RationalNumber(this, 1)
-
-fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(this.component1(), this.component2())
-
-data class RationalNumber(val numerator: Int, val denominator: Int)
